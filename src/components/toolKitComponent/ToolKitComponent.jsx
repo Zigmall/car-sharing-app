3eac1a5c7a2a7789c40c786bcb3807047a9fe33b
Rafal Hes <rafal.hes@yahoo.com>
2022-04-14 14:03:30 +0200

    Add ToolKitComponent

---
+++ src/components/toolKitComponent/ToolKitComponent.jsx
@@ -0,0 +1,13 @@
+import Rating from "./components/rating/Rating";
+import React, { useState } from 'react';
+
+
+
+const ToolKitComponent = () => {
+
+    return (
+        <Rating />
+    )
+}
+
+export default ToolKitComponent;
