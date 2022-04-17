de6d1e988ee17e945484cd6eabb0badafdd5ffc7
Rafal Hes <rafal.hes@yahoo.com>
2022-04-17 12:04:03 +0200

    Add Rating component

---
+++ src/components/rating/star/Star.jsx
@@ -0,0 +1,36 @@
+import React, { useMemo } from 'react';
+import styles from './Star.module.scss';
+import StarIcon from '../starIcon/StarIcon';
+import PropTypes from 'prop-types';
+
+const Star = ({ index, rating, hoveredNumber, onMouseEnter, onMouseLeave, onSave }) => {
+  const fill = useMemo(() => {
+    if (hoveredNumber >= index) {
+      return 'yellow';
+    } else if (!hoveredNumber && rating >= index) {
+      return 'yellow';
+    }
+    return 'none';
+  }, [rating, hoveredNumber, index]);
+
+  return (
+    <div
+      className={styles.stars}
+      onMouseEnter={() => onMouseEnter(index)}
+      onMouseLeave={() => onMouseLeave()}
+      onClick={() => onSave(index)}>
+      <StarIcon fill={fill} />
+    </div>
+  );
+};
+
+Star.propTypes = {
+  index: PropTypes.number,
+  rating: PropTypes.number,
+  hoveredNumber: PropTypes.number,
+  onMouseEnter: PropTypes.func,
+  onMouseLeave: PropTypes.func,
+  onSave: PropTypes.func
+};
+
+export default Star;
