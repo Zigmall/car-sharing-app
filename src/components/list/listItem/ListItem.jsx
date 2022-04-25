fef571be9805074cb583aa64d20cdd93b4981a4f
Rafal Hes <rafal.hes@yahoo.com>
2022-04-25 11:59:11 +0200

    Add styles to ListItem

--- src/components/list/listItem/ListItem.jsx
+++ src/components/list/listItem/ListItem.jsx
@@ -4,6 +4,25 @@ import styles from './ListItem.module.scss';
 const ListItem = () => {
   return (
     <div className={styles.wrapper}>
+      <div className={styles.topPart}>
+        <div className={styles.pictureSide}></div>
+        <div className={styles.iconSide}>
+          <div className={styles.carTypeAndName}>
+            <label>Economy</label>
+            <p>Ford Mustang</p>
+          </div>
+          <div className={styles.iconsLine}>
+            <p> ICON </p>
+            <p>5 seat </p>
+            <p> ICON </p>
+            <p>3 dors</p>
+            <p> ICON </p>
+          </div>
+        </div>
+      </div>
+      <hr></hr>
+      <div className={styles.bottomPart}></div>
+
       <p>List item</p>
     </div>
   );
