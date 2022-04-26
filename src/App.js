b519c2892e51d6c6a4b7fe5bf7f0ae36c444e494
Rafal Hes <rafal.hes@yahoo.com>
2022-04-26 16:46:25 +0200

    Add SvgList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import GroupWrapper from './components/groupElement/GroupWrapper';
-import ListItem from './components/list/listItem/ListItem';
+// import ListItem from './components/list/listItem/ListItem';
+
+import SvgIcon from './components/svgIcon/SvgIcon';
 
 const App = () => {
   return (
     <div className="App">
       <>
-        <GroupWrapper />
-        <ListItem />
+        {/* <ListItem carClass={'Economy'} /> */}
+        <SvgIcon iconHeight={'50'} iconWidth={'50'} />
       </>
     </div>
   );
