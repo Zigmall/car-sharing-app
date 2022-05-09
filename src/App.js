7b0d112552e801b33fd9a771aaf68a1bf85c37d1
Rafal Hes <rafal.hes@yahoo.com>
2022-05-09 10:47:23 +0200

    Move TopBar out of Router.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Home from './components/pages/home/Home';
 import { Route, Routes } from 'react-router';
 import CarDetails from './components/carDetails/CarDetails';
+import TopBar from './components/topBar/TopBar';
+import { toast } from 'react-toastify';
+import styles from './App.module.scss';
 
 const App = () => {
+  const [userLoggedIn, setUserLoggedIn] = useState(true);
+
+  const onLogout = () => {
+    setUserLoggedIn(false);
+  };
+
+  const notify = () =>
+    toast.success('You have been successfully logged out!', {
+      position: 'top-center',
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined
+    });
+
   return (
-    <div className="App">
+    <div className={styles.app}>
+      <TopBar userName="John Doe" onLogout={onLogout} userLoggedIn={userLoggedIn} notify={notify} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cars/:carId" element={<CarDetails iconHeight={'25'} iconWidth={'25'} />} />
