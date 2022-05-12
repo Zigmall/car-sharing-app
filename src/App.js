f32922070ff03f6d96768f26a25f5d18813f387f
Rafal Hes <rafal.hes@yahoo.com>
2022-05-12 15:54:45 +0200

    Add alert context to App component.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,27 @@ import Home from './components/pages/home/Home';
 import { Route, Routes } from 'react-router';
 import CarDetails from './components/carDetails/CarDetails';
 import styles from './App.module.scss';
-
 import CarState from './context/car/CarState';
 import Bars from './components/bars/Bars';
 import Registration from './components/auth/Registration';
+import AlertState from './context/alert/AlertState';
 
 const App = () => {
   return (
     <CarState>
-      <div className={styles.app}>
-        <Bars />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/registration" element={<Registration />} />
-          <Route path="/cars/:carId" element={<CarDetails iconHeight={'25'} iconWidth={'25'} />} />
-        </Routes>
-      </div>
+      <AlertState>
+        <div className={styles.app}>
+          <Bars />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/registration" element={<Registration />} />
+            <Route
+              path="/cars/:carId"
+              element={<CarDetails iconHeight={'25'} iconWidth={'25'} />}
+            />
+          </Routes>
+        </div>
+      </AlertState>
     </CarState>
   );
 };
