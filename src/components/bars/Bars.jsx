db975106f61a039667a1833bba524365e4a8fb1c
Rafal Hes <rafal.hes@yahoo.com>
2022-05-14 09:03:09 +0200

    Move currentView to Home component.

--- src/components/bars/Bars.jsx
+++ src/components/bars/Bars.jsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import TopBar from '../topBar/TopBar';
 import { toast } from 'react-toastify';
 import SideBar from '../sideBar/SideBar';
+import CarContext from '../../context/car/carContext';
 
 const Bars = () => {
   const [userLoggedIn, setUserLoggedIn] = useState(true);
   const [sideBarIndex, setSideBarIndex] = useState(1);
 
+  const carContext = useContext(CarContext);
+  const { getCars, divideCarsIntoPages, cars } = carContext;
+
   const onLogout = () => {
     setUserLoggedIn(false);
   };
+
+  useEffect(() => {
+    getCars();
+    if (cars !== null) {
+      divideCarsIntoPages(cars);
+    }
+  }, [cars]);
 
   const notify = () =>
     toast.success('You have been successfully logged out!', {
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,44 +1,35 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import List from '../../list/List';
 import CarContext from '../../../context/car/carContext';
 
 const Home = () => {
   const carContext = useContext(CarContext);
-  const { cars } = carContext;
+  const { currentView } = carContext;
+
   const viewElement = () => {};
 
-  const [currentIndex, setCurrentIndex] = useState(1);
-  const [numberOfPages, setNumberOfPages] = useState(null);
-  const [tableOfPages, setTableOfPages] = useState(null);
-  const [data, setData] = useState(cars);
+  // const [currentIndex, setCurrentIndex] = useState(1);
+  // const [numberOfPages, setNumberOfPages] = useState(null);
+  // const [tableOfPages, setTableOfPages] = useState(null);
 
-  useEffect(() => {
-    divideCarsIntoPages(cars);
-  }, []);
+  // const [data, setData] = useState(cars);
 
-  const divideCarsIntoPages = (car) => {
-    const copyOfDb = [...car];
-    const result = new Array(Math.ceil(copyOfDb.length / 10))
-      .fill()
-      .map(() => copyOfDb.splice(0, 10));
-    setTableOfPages(result);
-    setNumberOfPages(result.length);
-    setData(result[0]);
-  };
-
-  const changePage = (index) => {
-    setCurrentIndex(index);
-    setData(tableOfPages[index - 1]);
-  };
+  const changePage = () => {};
+  // const changePage = (index) => {
+  //   setCurrentIndex(index);
+  //   setData(tableOfPages[index - 1]);
+  // };
   return (
     <>
-      <List
-        data={data}
-        viewElement={viewElement}
-        currentIndex={currentIndex}
-        numberOfPages={numberOfPages}
-        changePage={changePage}
-      />
+      {currentView && (
+        <List
+          data={currentView}
+          viewElement={viewElement}
+          currentIndex={1}
+          numberOfPages={1}
+          changePage={changePage}
+        />
+      )}
     </>
   );
 };
