f84e00eed9098d6d9c6f25f58c08f8473575648d
Rafal Hes <rafal.hes@yahoo.com>
2022-06-02 21:56:34 +0200

    Add mechanics to ActionButtons

--- src/components/actionButton/ActionButtons.jsx
+++ src/components/actionButton/ActionButtons.jsx
@@ -2,35 +2,38 @@ import React, { useContext, useEffect } from 'react';
 import BorrowActionButton from './BorrowActionButton';
 import PropTypes from 'prop-types';
 import AlertContext from '../../context/alert/alertContext';
+import ReturnActionButton from './ReturnActionButton';
 
-const ActionButtons = ({ carCopy }) => {
+const ActionButtons = ({ carCopy, returnCar }) => {
   const alertContext = useContext(AlertContext);
   const { setAlert } = alertContext;
 
-  const canBorrow = !!carCopy.copies.filter((car) => car.borrower === null).length > 0;
+  let availableCopies = null;
+  let canBorrow = null;
+  if (!returnCar) {
+    availableCopies = carCopy.copies.filter((car) => car.borrower === null);
+    canBorrow = !!availableCopies.length > 0;
+  }
+
   //   const canReturn = !carCopy.borrower;
+  // const carsToReturn =
 
   useEffect(() => {
-    if (carCopy.copies.filter((car) => car.borrower === null).length === 0) {
-      console.log(123);
+    if (availableCopies !== null && availableCopies.length === 0 && !returnCar) {
       setAlert('Car temporary unavailable', 'danger');
     }
   }, []);
   return (
     <div>
-      <>
-        {canBorrow && (
-          <BorrowActionButton
-            availableCarCopy={carCopy.copies.filter((car) => car.borrower === null)[0].id}
-          />
-        )}
-      </>
+      {canBorrow && <BorrowActionButton availableCarCopy={availableCopies[0].id} />}
+      {returnCar && <ReturnActionButton borrowedCarCopy={carCopy} />}
     </div>
   );
 };
 
 ActionButtons.propTypes = {
-  carCopy: PropTypes.object
+  carCopy: PropTypes.object,
+  returnCar: PropTypes.bool
 };
 
 export default ActionButtons;
