2c1c23534eb7eed2824d820f08f9ff0c908877a3
Rafal Hes <rafal.hes@yahoo.com>
2022-06-02 21:55:16 +0200

    Add ReturnCars component.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import Registration from './components/auth/Registration';
 import AlertState from './context/alert/AlertState';
 import Alerts from './components/alerts/Alerts';
 import Login from './components/auth/Login';
+import ReturnCars from './components/pages/returnCars/ReturnCars';
 import { ApolloClient, InMemoryCache, HttpLink, ApolloProvider } from '@apollo/client';
+
 // const URL = 'https://desolate-spire-04068.herokuapp.com';
 const URL = 'http://localhost:5000';
 const client = new ApolloClient({
@@ -32,6 +34,7 @@ const App = () => {
               <Route path="/registration" element={<Registration />} />
               <Route path="login" element={<Login />} />
               <Route path="/cars/:carId" element={<CarDetails />} />
+              <Route path="/returnCars" element={<ReturnCars />} />
             </Routes>
           </div>
         </AlertState>
---
+++ src/components/pages/returnCars/ReturnCars.jsx
@@ -0,0 +1,65 @@
+import { gql, useQuery } from '@apollo/client';
+import React from 'react';
+import styles from './ReturnCars.module.scss';
+import Car from '../../car/Car';
+
+const GET_ALL_BORROWED_CARS = gql`
+  query ($userId: ID!) {
+    user(id: $userId) {
+      borrowedCarCopies {
+        id
+        car {
+          id
+          carClass
+          benefits
+          model
+          brand {
+            name
+          }
+          year
+          property {
+            seats
+            doors
+            trunk
+            airConditioning
+            manualGearBox
+          }
+          location
+          price
+          copies {
+            id
+            borrower {
+              id
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const ReturnCars = () => {
+  const { data } = useQuery(GET_ALL_BORROWED_CARS, { variables: { userId: 'VXNlci0y' } });
+  return (
+    <div className={styles.returnCarWrapper}>
+      {data &&
+        data.user.borrowedCarCopies.map((car, index) => (
+          <Car
+            key={index}
+            id={car.car.id}
+            carClass={car.car.carClass}
+            benefits={car.car.benefits}
+            model={car.car.model}
+            brand={car.car.brand}
+            property={car.car.property}
+            location={car.car.location}
+            price={car.car.price}
+            carCopy={car}
+            returnCar={true}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default ReturnCars;
