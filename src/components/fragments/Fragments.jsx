f31de0eb4a023ee5426601d35b4da4c63c5b6c90
Rafal Hes <rafal.hes@yahoo.com>
2022-06-03 19:10:29 +0200

    Create Fragments and add them to Fragment.jsx

---
+++ src/components/fragments/Fragments.jsx
@@ -0,0 +1,49 @@
+import { gql } from '@apollo/client';
+
+export const AVATAR_FRAGMENT = gql`
+  fragment avatarFields on Avatar {
+    image {
+      url
+    }
+    color
+  }
+`;
+
+export const USER_FRAGMENT = gql`
+  fragment userFields on User {
+    id
+    firstName
+    lastName
+    email
+    mobile
+    address {
+      city
+      street
+      houseNo
+      flatNo
+      postCode
+    }
+    avatar {
+      ...avatarFields
+    }
+  }
+  ${AVATAR_FRAGMENT}
+`;
+
+export const CAR_COPY_FRAGMENT = gql`
+  fragment carCopyFields on CarCopy {
+    id
+    car {
+      id
+    }
+    borrower {
+      id
+      firstName
+      lastName
+      avatar {
+        ...avatarFields
+      }
+    }
+  }
+  ${AVATAR_FRAGMENT}
+`;
