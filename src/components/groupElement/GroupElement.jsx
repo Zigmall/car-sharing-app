399785971689a9d3bc92efcfffe578a13e9ff0f3
Rafal Hes <rafal.hes@yahoo.com>
2022-07-05 10:46:03 +0200

    Remove two middle icons from GrouElement item.

--- src/components/groupElement/GroupElement.jsx
+++ src/components/groupElement/GroupElement.jsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import styles from './TypeNavigationBar.module.scss';
-import InfoIcon from './InfoIcon';
+// import InfoIcon from './InfoIcon';
 import PropTypes from 'prop-types';
 import MiddleIcon from './MiddleIcon';
 
-const GroupElement = ({ model, active, luggage, passengers, price, onClickItem }) => {
+const GroupElement = ({ model, active, price, onClickItem }) => {
   return (
     <div onClick={onClickItem} className={styles.wrapper}>
       <div className={styles.header}>
         <p>{model}</p>
       </div>
-      <div className={styles.iconsGroup}>
-        <InfoIcon icon="passengers" />
-        <p>{passengers}</p>
-        <InfoIcon icon="luggage" />
-        <p>{luggage}</p>
-      </div>
       <div className={styles.carIcon}>
         <MiddleIcon model={model} />
       </div>
+
       <div className={styles.price}>
         <p>From €{price}/day</p>
       </div>
