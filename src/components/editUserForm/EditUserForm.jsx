c56b4fb469618e2495ad0ed4a158d4e0363ac9d8
Rafal Hes <rafal.hes@yahoo.com>
2022-07-10 20:37:10 +0200

    Create EditUserForm file.

---
+++ src/components/editUserForm/EditUserForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './EditUserForm.module.scss';
+
+const EditUserForm = () => {
+  return (
+    <>
+      <div className={styles.edit__user__wrapper}>EditUserForm</div>
+    </>
+  );
+};
+
+export default EditUserForm;
