98c1d982afb742faaf0da19a7187dbc7cb7e5de8
Rafal Hes <rafal.hes@yahoo.com>
2022-07-13 20:55:27 +0200

    Add mobile and isAdmin fields to EditUserForm

--- src/components/editUserForm/EditUserForm.jsx
+++ src/components/editUserForm/EditUserForm.jsx
@@ -7,23 +7,31 @@ import { GET_ALL_USERS } from '../../queries/queries';
 import AlertContext from '../../context/alert/alertContext';
 
 const EditUserForm = ({ user }) => {
+  console.log('user', user);
   const alertContext = useContext(AlertContext);
   const { setAlert } = alertContext;
   const [firstName, setFirstName] = useState(user.firstName);
   const [lastName, setLastName] = useState(user.lastName);
   const [email, setEmail] = useState(user.email);
+  const [mobile, setMobile] = useState(user.mobile);
+  const [isAdmin, setIsAdmin] = useState(user.isAdmin);
 
   const [updateUser] = useMutation(UPDATE_USER, {
-    variables: { input: { id: user.id, firstName, lastName, email } },
+    variables: { input: { id: user.id, firstName, lastName, email, mobile, isAdmin } },
     refetchQueries: [{ query: GET_ALL_USERS }]
   });
 
+  const handleCheckboxChange = () => {
+    setIsAdmin(!isAdmin);
+  };
+
   const handleUpdateUser = (e) => {
     e.preventDefault();
-    if (firstName === '' || lastName === '' || email === '') {
+    if ((firstName === '' || lastName === '' || email === '', mobile === '')) {
       setAlert('Please fill out all fields', 'warning');
     } else {
       setAlert('User updated', 'success');
+      console.log('values >>>', firstName, lastName, email, mobile, isAdmin);
       updateUser();
     }
   };
@@ -57,7 +65,8 @@ const EditUserForm = ({ user }) => {
                 />
               </div>
             </div>
-            <div className={styles.form__email}>
+
+            <div className={styles.name__group}>
               <div className={styles.form__element}>
                 <label className={styles.form__label}>Email</label>
                 <input
@@ -68,6 +77,28 @@ const EditUserForm = ({ user }) => {
                   onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
+
+              <div className={styles.form__element}>
+                <label className={styles.form__label}>Mobile</label>
+                <input
+                  type="text"
+                  className={styles.form__input}
+                  id="mobile"
+                  value={mobile}
+                  onChange={(e) => setMobile(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className={styles.admin__wrapper}>
+              <input
+                type="checkbox"
+                className={styles.admin__checkbox}
+                id="admin"
+                checked={isAdmin}
+                onChange={() => handleCheckboxChange()}
+              />
+              <label className={styles.form__label}>Administrator</label>
             </div>
 
             <button type="submit" className={styles.button__update}>
