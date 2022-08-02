9a233ba034be845176cc18b537910585c47547e7
Rafal Hes <rafal.hes@yahoo.com>
2022-08-02 19:32:32 +0200

    Remove outdated functions from ActionButtons.

--- src/components/actionButton/ActionButtons.jsx
+++ src/components/actionButton/ActionButtons.jsx
@@ -1,28 +1,14 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import BorrowActionButton from './BorrowActionButton';
 import PropTypes from 'prop-types';
-import AlertContext from '../../context/alert/alertContext';
 import ReturnActionButton from './ReturnActionButton';
 
 const ActionButtons = ({ car, returnCar }) => {
-  const alertContext = useContext(AlertContext);
-  const { setAlert } = alertContext;
+  console.log(car);
 
-  let availableCopies = null;
-  let canBorrow = null;
-  if (!returnCar) {
-    availableCopies = car.copies.filter((car) => car.borrower === null);
-    canBorrow = !!availableCopies.length > 0;
-  }
-
-  useEffect(() => {
-    if (availableCopies !== null && availableCopies.length === 0 && !returnCar) {
-      setAlert('Car temporary unavailable', 'danger');
-    }
-  }, []);
   return (
     <div>
-      {canBorrow && <BorrowActionButton availableCarCopy={availableCopies[0].id} />}
+      {!returnCar && <BorrowActionButton carId={car.id} />}
       {returnCar && <ReturnActionButton car={car} />}
     </div>
   );
