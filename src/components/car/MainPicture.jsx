4d93247a157d3b94e154678a9aa44f60870d2146
Rafal Hes <rafal.hes@yahoo.com>
2022-11-25 13:01:31 +0100

    Destructure url from prop in MainPicture component

--- src/components/car/MainPicture.jsx
+++ src/components/car/MainPicture.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import styles from './Car.module.scss';
 import PropTypes from 'prop-types';
 
-const MainPicture = ({ picturePath: { url } }) => {
+const MainPicture = ({ picturePath }) => {
   return (
     <div className={styles.mainPictureWrapper}>
-      <img src={url} alt="Car" className={styles.mainPicture} />
+      <img src={picturePath} alt="Car" className={styles.mainPicture} />
     </div>
   );
 };
 
 MainPicture.propTypes = {
-  picturePath: PropTypes.object
+  picturePath: PropTypes.string
 };
 
 export default MainPicture;
