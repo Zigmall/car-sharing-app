5c63bb4d5fadfab1f4dd20ee75b2c260ba61e695
Rafal Hes <rafal.hes@yahoo.com>
2022-11-27 11:32:48 +0100

    Remove react-calendar and add react-datepicker.

--- src/components/pages/order/OrderCar.jsx
+++ src/components/pages/order/OrderCar.jsx
@@ -1,17 +1,15 @@
 import React, { useState } from 'react';
-import Calendar from 'react-calendar';
 import styles from './OrderCar.module.scss';
 import 'react-calendar/dist/Calendar.css';
 
 const OrderCar = () => {
-  const [date, setDate] = useState(new Date());
+  // const [date, setDate] = useState(new Date());
 
-  const handleDateChange = (e) => setDate(e);
+  // const handleDateChange = (e) => setDate(e);
 
   return (
     <div className={styles.calendar__wrapper}>
-      <Calendar onChange={handleDateChange} value={date} />
-      {console.log(date)}
+      <p>Order car</p>
     </div>
   );
 };
