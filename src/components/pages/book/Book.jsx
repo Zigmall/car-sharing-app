336533484546e25e055bc13e4cd6da8826ed8657
Rafal Hes <rafal.hes@yahoo.com>
2022-11-28 19:58:38 +0100

    Get user data from context instead query.

--- src/components/pages/book/Book.jsx
+++ src/components/pages/book/Book.jsx
@@ -1,61 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import DatePicker from 'react-datepicker';
 import styles from './Book.module.scss';
 import 'react-datepicker/dist/react-datepicker.css';
 import EditUserForm from '../../editUserForm/EditUserForm';
+import AuthContext from '../../../context/auth/authContext';
 
 const Book = () => {
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
-  console.log('Time:', startDate);
+  // const navigate = useNavigate();
+  // const carContext = useContext(CarContext);
+  // const { changeTab } = carContext;
+  const authContext = useContext(AuthContext);
+  const { user } = authContext;
   return (
     <>
-      <div className={styles.page__wrapper}>
-        <div className={styles.userData__title}>
-          <h1>Car booking</h1>
-        </div>
-        <div className={styles.userData__steps}>
-          <h2>Step 1: Complete your personal data</h2>
-        </div>
-        <div className={styles.userData__wrapper}>
-          <EditUserForm completeForBooking={true} />
-        </div>
-        <div className={styles.userData__steps}>
-          <h2>Step 2: Pick start and end dates</h2>
-        </div>
-        <div className={styles.datePicker__container}>
-          <div className={styles.datePicker__container__left}>
-            <label>Start Date</label>
+      {user && (
+        <div className={styles.page__wrapper}>
+          <div className={styles.userData__title}>
+            <h1>Car booking</h1>
+          </div>
+          <div className={styles.userData__steps}>
+            <h2>Step 1: Complete your personal data</h2>
+          </div>
+          <div className={styles.userData__wrapper}>
+            <EditUserForm user={user} completeForBooking={true} />
+          </div>
+          <div className={styles.userData__steps}>
+            <h2>Step 2: Pick start and end dates</h2>
+          </div>
+          <div className={styles.datePicker__container}>
+            <div className={styles.datePicker__container__left}>
+              <label>Start Date</label>
 
-            <DatePicker
-              showTimeSelect
-              selected={startDate}
-              onChange={(date) => setStartDate(date)}
-              selectsStart
-              startDate={startDate}
-              endDate={endDate}
-              minDate={new Date()}
-              dateFormat="d/M/yyyy - HH:mm"
-            />
+              <DatePicker
+                showTimeSelect
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                selectsStart
+                startDate={startDate}
+                endDate={endDate}
+                minDate={new Date()}
+                dateFormat="d/M/yyyy - HH:mm"
+              />
+            </div>
+            <div className={styles.datePicker__container__right}>
+              <label>End Date</label>
+              <DatePicker
+                showTimeSelect
+                selected={endDate}
+                onChange={(date) => setEndDate(date)}
+                selectsEnd
+                startDate={startDate}
+                endDate={endDate}
+                minDate={startDate}
+                dateFormat="d/M/yyyy - HH:mm"
+              />
+            </div>
           </div>
-          <div className={styles.datePicker__container__right}>
-            <label>End Date</label>
-            <DatePicker
-              showTimeSelect
-              selected={endDate}
-              onChange={(date) => setEndDate(date)}
-              selectsEnd
-              startDate={startDate}
-              endDate={endDate}
-              minDate={startDate}
-              dateFormat="d/M/yyyy - HH:mm"
-            />
+          <div className={styles.userData__steps}>
+            <h2>Step 3: Confirm all data by pressing BOOK button</h2>
           </div>
         </div>
-        <div className={styles.userData__steps}>
-          <h2>Step 3: Confirm all data by pressing BOOK button</h2>
-        </div>
-      </div>
+      )}
     </>
   );
 };
