748c3e4a4ad9a38e6ad144d009267c37c853ad8f
Rafal Hes <rafal.hes@yahoo.com>
2022-11-28 19:59:33 +0100

    Remove redundant queries in User component.

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './User.module.scss';
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { GET_ALL_USERS } from '../../queries/queries';
+// import { useParams } from 'react-router-dom';
+// import { useQuery } from '@apollo/client';
+// import { GET_USER } from '../../queries/queries';
 import EditUserForm from '../editUserForm/EditUserForm';
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
@@ -10,30 +10,14 @@ import CarContext from '../../context/car/carContext';
 import AuthContext from '../../context/auth/authContext';
 
 const User = () => {
-  const { userId } = useParams();
-  const { loading, error, data } = useQuery(GET_ALL_USERS);
   const navigate = useNavigate();
   const carContext = useContext(CarContext);
   const { changeTab } = carContext;
   const authContext = useContext(AuthContext);
-  const { user: currentUser } = authContext;
-
-  if (loading) return <p>Loading...</p>;
-  if (error) {
-    console.log(error);
-    return (
-      <div className={styles.left__space}>
-        <div className={styles.error__message}>
-          <p>Something went wrong</p>
-        </div>
-      </div>
-    );
-  }
-
-  const user = data.allUsers.filter((user) => user.id === userId);
+  const { user } = authContext;
 
   const goBack = () => {
-    if (currentUser.isAdmin) {
+    if (user.isAdmin) {
       navigate('/users');
     } else {
       changeTab(1);
@@ -42,7 +26,7 @@ const User = () => {
   };
   return (
     <>
-      {data && (
+      {user && (
         <div className={styles.left__space}>
           <div className={styles.user__wrapper}>
             <div className={styles.button__wrapper}>
@@ -55,7 +39,7 @@ const User = () => {
             </div>
             <h1>Update personal data</h1>
             <div className={styles.user__form}>
-              <EditUserForm completeForBooking={false} />
+              <EditUserForm user={user} completeForBooking={false} />
             </div>
           </div>
         </div>
