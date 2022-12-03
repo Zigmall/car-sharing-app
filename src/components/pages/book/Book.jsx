0ece7cf88795ed0b3def4d490c8eab495bfd696d
Rafal Hes <rafal.hes@yahoo.com>
2022-12-03 17:19:06 +0100

    Add two steps: Insurance and payment in Book.

--- src/components/pages/book/Book.jsx
+++ src/components/pages/book/Book.jsx
@@ -12,8 +12,11 @@ const Book = () => {
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [showEditUserForm, setShowEditUserForm] = useState(true);
-  // const [showDatePicker, setShowDatePicker] = useState(false);
-
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [showInsuranceOptions, setShowInsuranceOptions] = useState(false);
+  const [showPaymentSummary, setShowPaymentSummary] = useState(false);
+  const [insuranceRate, setInsuranceRate] = useState(0.8);
+  // const [insuranceType, setInsuranceType] = useState('Basic');
   const { carId } = useParams();
   const { loading, error, data } = useQuery(GET_CAR, {
     variables: { carId }
@@ -43,6 +46,19 @@ const Book = () => {
 
   const checkIfUserHasAllData = (completed) => {
     setShowEditUserForm(!completed);
+    setShowDatePicker(completed);
+  };
+
+  const calculateInsurancePrice = () => {
+    return Math.round(Math.ceil((car.price * (endDate - startDate)) / 86400000) * insuranceRate, 2);
+  };
+
+  const calculateTotalPrice = () => {
+    return Math.round(
+      Math.ceil((car.price * (endDate - startDate)) / 86400000) +
+        calculateInsurancePrice(insuranceRate),
+      2
+    );
   };
 
   return (
@@ -53,7 +69,7 @@ const Book = () => {
             <h1>Car booking</h1>
           </div>
           <div className={styles.userData__steps}>
-            <h2>{`Step 1: Complete your personal data ${
+            <h2>{`Step 1: Your personal data: ${
               showEditUserForm ? 'INCOMPLETE' : 'COMPLETED'
             }`}</h2>
           </div>
@@ -66,14 +82,15 @@ const Book = () => {
               />
             </div>
           )}
-          {!showEditUserForm && (
-            <>
-              <div className={styles.userData__steps}>
-                <h2>{`Step 2: Pick start and end dates ${
-                  startDate && endDate ? 'COMPLETED' : ''
-                }`}</h2>
-              </div>
 
+          {(showDatePicker || showPaymentSummary) && (
+            <div className={styles.userData__steps}>
+              <h2>{`Step 2: Start and end dates ${startDate && endDate ? 'COMPLETED' : ''}`}</h2>
+            </div>
+          )}
+
+          {showDatePicker && (
+            <>
               <div className={styles.datePicker__container}>
                 <div className={styles.datePicker__container__left}>
                   <label>Start Date</label>
@@ -81,7 +98,13 @@ const Book = () => {
                   <DatePicker
                     showTimeSelect
                     selected={startDate}
-                    onChange={(date) => setStartDate(date)}
+                    onChange={(date) => {
+                      setStartDate(date);
+                      setShowPaymentSummary(startDate && endDate ? true : false);
+                      setShowDatePicker(startDate && endDate ? false : true);
+                      setShowInsuranceOptions(startDate && endDate ? true : false);
+                      console.log(startDate && endDate ? true : false);
+                    }}
                     selectsStart
                     isClearable
                     startDate={startDate}
@@ -95,7 +118,13 @@ const Book = () => {
                   <DatePicker
                     showTimeSelect
                     selected={endDate}
-                    onChange={(date) => setEndDate(date)}
+                    onChange={(date) => {
+                      setEndDate(date);
+                      setShowPaymentSummary(startDate && endDate ? true : false);
+                      setShowDatePicker(startDate && endDate ? false : true);
+                      setShowInsuranceOptions(startDate && endDate ? true : false);
+                      console.log(startDate && endDate ? true : false);
+                    }}
                     selectsEnd
                     isClearable
                     startDate={startDate}
@@ -107,18 +136,69 @@ const Book = () => {
               </div>
             </>
           )}
-          {!showEditUserForm && startDate && endDate && (
+          {showInsuranceOptions && (
+            <div className={styles.userData__steps}>
+              <h2>Step 3: Insurance options</h2>
+            </div>
+          )}
+          {showPaymentSummary && (
             <>
               <div className={styles.userData__steps}>
-                <h2>Step 3: Confirm all data by pressing BOOK button</h2>
+                <h2>Step 4: Payment</h2>
               </div>
-              <div className={styles.summary__wrapper}>
-                <button onClick={() => handleSummaryButton()} className={styles.summary__button}>
-                  BOOK
-                </button>
+              <div className={styles.payment__summary}>
+                <div className={styles.payment__summary__left}>
+                  <div className={styles.payment__summary__left__title}>
+                    <h3>Car</h3>
+                  </div>
+                  <div className={styles.payment__summary__left__car}>
+                    <div className={styles.payment__summary__left__car__image}>
+                      <img
+                        src={
+                          car.picturePath.url
+                            ? car.picturePath.url
+                            : 'https://via.placeholder.com/150'
+                        }
+                        alt="car"
+                      />
+                    </div>
+                    <div className={styles.payment__summary__left__car__info}>
+                      <p>{car.brand.name}</p>
+                      <h3>{car.model}</h3>
+                      <p>{car.description}</p>
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.payment__summary__right}>
+                  <div className={styles.payment__summary__right__title}>
+                    <h3>Payment</h3>
+                  </div>
+                  <div className={styles.payment__summary__right__info}>
+                    <div className={styles.payment__summary__right__info__left}>
+                      <p>Start Date</p>
+                      <p>End Date</p>
+                      <p>Price per day</p>
+                      <p>Insurance</p>
+                      <p>Total price</p>
+                    </div>
+                    <div className={styles.payment__summary__right__info__right}>
+                      <p>{startDate && startDate.toLocaleString()}</p>
+                      <p>{endDate && endDate.toLocaleString()}</p>
+                      <p>€{car.price}</p>
+                      <p>€{calculateInsurancePrice()}</p>
+                      <p>€{calculateTotalPrice()}</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </>
           )}
+
+          <div className={styles.summary__wrapper}>
+            <button onClick={() => handleSummaryButton()} className={styles.summary__button}>
+              BOOK
+            </button>
+          </div>
         </div>
       )}
     </>
