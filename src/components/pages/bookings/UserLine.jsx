17c48349eb666420b43ff351e0fe3dde08e14e07
Rafal Hes <rafal.hes@yahoo.com>
2022-12-10 13:16:51 +0100

    Fix UserLine props.

--- src/components/pages/bookings/UserLine.jsx
+++ src/components/pages/bookings/UserLine.jsx
@@ -6,10 +6,10 @@ import PropTypes from 'prop-types';
 // import { useMutation } from '@apollo/client';
 import { useNavigate } from 'react-router-dom';
 
-const UserLine = ({ user }) => {
+const UserLine = ({ booking: { id, booker, startDate, endDate, car } }) => {
   const navigate = useNavigate();
   const handleRent = () => {
-    navigate(`/users/${user.id}`);
+    navigate(`/bookings/${id}`);
   };
 
   const cancelBooking = () => {
@@ -19,9 +19,10 @@ const UserLine = ({ user }) => {
   return (
     <>
       <tr>
-        <td>{`${user.firstName} ${user.lastName}`}</td>
-        <td>{user.email}</td>
-        <td>{user.mobile}</td>
+        <td>{`${booker.firstName} ${booker.lastName}`}</td>
+        <td>{startDate}</td>
+        <td>{endDate}</td>
+        <td>{`${car.brand.name} ${car.model}`}</td>
         <td>
           <button
             onClick={() => handleRent()}
@@ -42,11 +43,11 @@ const UserLine = ({ user }) => {
 };
 
 UserLine.propTypes = {
-  user: PropTypes.object,
-  firstName: PropTypes.string,
-  lastName: PropTypes.string,
-  email: PropTypes.string,
-  mobile: PropTypes.string
+  booking: PropTypes.object.isRequired,
+  booker: PropTypes.object.isRequired,
+  startDate: PropTypes.string.isRequired,
+  endDate: PropTypes.string.isRequired,
+  car: PropTypes.object.isRequired
 };
 
 export default UserLine;
