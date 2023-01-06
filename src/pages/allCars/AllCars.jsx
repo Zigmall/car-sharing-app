5ea0f031ad84602c2f3241428ecc552c8ad48631
Rafal Hes <rafal.hes@yahoo.com>
2023-01-06 16:16:32 +0100

    Add AllCars component.

--- src/pages/allCars/AllCars.jsx
+++ src/pages/allCars/AllCars.jsx
@@ -1,41 +1,83 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import styles from './AllCars.module.scss';
 import { GET_BRANDS } from '../../queries/queries';
 import { useQuery } from '@apollo/client';
 import CarRow from './CarRow';
+import AuthContext from '../../context/auth/authContext';
 
 const AllCars = () => {
-  const { loading, error, data } = useQuery(GET_BRANDS);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :</p>;
+  const [brands, setBrands] = useState();
+  const authContext = useContext(AuthContext);
+  const { user } = authContext;
+  const { loading, error, data } = useQuery(GET_BRANDS, {
+    onCompleted: (data) => {
+      setBrands(data.brands);
+    }
+  });
+  if (loading)
+    return (
+      <div className={styles.error__message}>
+        <p>Loading...</p>
+      </div>
+    );
+
+  if (error) {
+    console.log(error);
+    if (user === null) {
+      return (
+        <div className={styles.error__message}>
+          <p>You need to be log in to see this page</p>
+        </div>
+      );
+    }
+    return (
+      <div className={styles.error__message}>
+        <p>Something went wrong...</p>
+      </div>
+    );
+  }
 
   return (
     <>
-      <div className={styles.left__space}>
-        <div className={styles.rentals__wrapper}>
-          {data &&
-            data.brands.map((brand) => {
-              return (
-                <div key={brand.id} className={styles.brand__item}>
-                  <h1>{brand.name}</h1>
-                  <table>
-                    <thead>
-                      <tr>
-                        <th>Model</th>
-                        <th>ID</th>
-                        <th>Year</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {brand.cars.length > 0 &&
-                        brand.cars.map((car) => <CarRow key={car.id} car={car} />)}
-                    </tbody>
-                  </table>
-                </div>
-              );
-            })}
+      {data && brands && user && !(user.role === 'ADMIN' || user.role === 'SUPERVISOR') ? (
+        <div className={styles.error__message}>
+          <h4>You need to be higher rank to perform this action</h4>
         </div>
-      </div>
+      ) : (
+        <>
+          {data &&
+            brands &&
+            brands.length > 0 &&
+            brands.map(
+              (brand) =>
+                brand.cars.length > 0 && (
+                  <div className={styles.left__space}>
+                    <div className={styles.rentals__wrapper}>
+                      <div key={brand.id} className={styles.brand__item}>
+                        <h1>{brand.name}</h1>
+                        <table>
+                          <thead>
+                            <tr>
+                              <th>Picture</th>
+                              <th>Car</th>
+                              <th>ID</th>
+                              <th>Status</th>
+                              <th>Update</th>
+                              <th>Delete</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {brand.cars.length > 0 &&
+                              brand.cars.map((car) => <CarRow key={car.id} car={car} />)}
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                )
+            )}
+        </>
+      )}
     </>
   );
 };
