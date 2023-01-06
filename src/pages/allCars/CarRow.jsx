a011698e1b814c35877fb7b70cd23c62d1b92581
Rafal Hes <rafal.hes@yahoo.com>
2023-01-06 16:16:51 +0100

    Add CarRow component.

--- src/pages/allCars/CarRow.jsx
+++ src/pages/allCars/CarRow.jsx
@@ -1,29 +1,68 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+import styles from './AllCars.module.scss';
+import { GET_BRANDS } from '../../queries/queries';
+import { useMutation } from '@apollo/client';
+import { DELETE_CAR } from '../../mutations/mutations';
+import AlertContext from '../../context/alert/alertContext';
 
-const CarRow = ({ car: { model, id, year } }) => {
+const CarRow = ({ car: { picturePath, brand, model, id } }) => {
+  const navigate = useNavigate();
+  const alertContext = useContext(AlertContext);
+  const { setAlert } = alertContext;
+  const handleEdit = (id) => {
+    navigate(`/edit-car/${id}`);
+  };
+
+  const handleDelete = (id) => {
+    const txt = 'Are you sure you want to delete this car?';
+    confirm(txt) && deleteCar({ variables: { carId: id } });
+  };
+
+  const [deleteCar] = useMutation(DELETE_CAR, {
+    onCompleted: ({ deleteCar: { success, message } }) => {
+      if (success) {
+        setAlert('Car deleted successfully', 'success');
+      } else {
+        setAlert('Something went wrong', 'danger');
+        console.log('error:', message);
+      }
+    },
+    onError: (error) => {
+      console.log('error:', error.message);
+    },
+    refetchQueries: [{ query: GET_BRANDS }]
+  });
+
   return (
     <>
-      <tr>
-        <td>{model}</td>
-        <td>{id}</td>
-        <td>{year}</td>
-        {/* <td>{car.borrowed ? 'Yes' : 'No'}</td> */}
-        {/* <td>
-          <button
-            onClick={handleEdit}
-            className={[style.button__wrapper, style.button__edit].join(' ')}>
-            Edit User
-          </button>
-        </td>
-        <td>
-          <button
-            onClick={handleDelete}
-            className={[style.button__wrapper, style.button__delete].join(' ')}>
-            Delete User
-          </button>
-        </td> */}
-      </tr>
+      {id && (
+        <tr>
+          <td>
+            <div className={styles.spallPictureWrapper}>
+              <img src={picturePath.url} alt="Car" className={styles.spallPicture} />
+            </div>
+          </td>
+          <td>{`${brand.name} ${model}`}</td>
+          <td>{id}</td>
+          <td>Status</td>
+          <td>
+            <button
+              onClick={() => handleEdit(id)}
+              className={[styles.button__wrapper, styles.button__update].join(' ')}>
+              Update Car
+            </button>
+          </td>
+          <td>
+            <button
+              onClick={() => handleDelete(id)}
+              className={[styles.button__wrapper, styles.button__cancelBooking].join(' ')}>
+              Delete Car
+            </button>
+          </td>
+        </tr>
+      )}
     </>
   );
 };
@@ -31,8 +70,7 @@ const CarRow = ({ car: { model, id, year } }) => {
 CarRow.propTypes = {
   car: PropTypes.object,
   model: PropTypes.string,
-  id: PropTypes.string,
-  year: PropTypes.number
+  id: PropTypes.string
 };
 
 export default CarRow;
