bd5d4f2dce1b2b817d9837f7d9b7c0c9dad25aab
Rafal Hes <rafal.hes@yahoo.com>
2023-01-24 15:47:19 +0100

    Complete MyRent component.

--- src/pages/myRents/MyRents.jsx
+++ src/pages/myRents/MyRents.jsx
@@ -3,11 +3,9 @@ import styles from './MyRents.module.scss';
 import { useQuery } from '@apollo/client';
 import { GET_RENTS_BY_USER_ID } from '../../queries/queries';
 import AuthContext from '../../context/auth/authContext';
-import { useNavigate } from 'react-router-dom';
-import RentLine from '../../pages/rents/RentLine';
+import UserRentListElement from './UserRentListElement';
 
 const MyRents = () => {
-  const navigate = useNavigate();
   const authContext = useContext(AuthContext);
   const { user } = authContext;
   const { loading, error, data } = useQuery(GET_RENTS_BY_USER_ID);
@@ -34,9 +32,6 @@ const MyRents = () => {
       </div>
     );
   }
-  const displayHandlingOverCard = (id) => {
-    navigate(`/rents/handling-over-card/${id}`);
-  };
 
   return (
     <>
@@ -46,19 +41,18 @@ const MyRents = () => {
             <table>
               <thead>
                 <tr>
-                  <th>Client Name</th>
                   <th>Start Date</th>
                   <th>Pick up Day</th>
                   <th>End Date</th>
                   <th>Return Day</th>
                   <th>Car</th>
-                  <th>Rent</th>
-                  <th>Action</th>
+                  <th>Handling Over Card</th>
+                  <th>Status</th>
                 </tr>
               </thead>
               <tbody>
                 {data.rentsByUserId.map((rent) => {
-                  return <RentLine key={rent.id} rent={rent} />;
+                  return <UserRentListElement key={rent.id} rent={rent} />;
                 })}
               </tbody>
             </table>
